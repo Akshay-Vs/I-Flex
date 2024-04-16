8128f506ed5b66f0aa60e129fd79185b30a59ff1
Akshay Vs <me.akshay.vs@gmail.com>
2024-04-16 16:23:39 +0530

    feat: initial server configuration

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
